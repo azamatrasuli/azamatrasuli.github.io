7913636d9b8ddd36f072c904135ab6b879ae220e
azamatrasuli <rslazamat.dev@proton.me>
2024-08-20 04:32:26 +0300

    up

---
+++ index.js
@@ -0,0 +1,11 @@
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+export {
+  backend,
+  creator,
+  mobile,
+  web,
+};
